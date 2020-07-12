6413dda5344e0e29a70756d7680115fe8fce85e5
phantware <phantmoney2011@gmail.com>
2020-07-12 17:47:42 +0100

    Read me updated

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID:
+    '192697751251-s4bi8j0tq8h3nrh4h490c9co8mu1r9ao.apps.googleusercontent.com',
+  googleClientSecret: 'djRjk40W4AYIQFXzVBK42UcG',
+  mongooseURI: 'mongodb://localhost:27017/emaily',
+  cookieKey: 'babajamotokusecretcookie',
+};
