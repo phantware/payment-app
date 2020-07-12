e96718b9f7794c30e8474b79c75a7819d90fa751
Ismail Jamiu Babatunde <32511206+phantware@users.noreply.github.com>
2020-07-12 22:03:26 +0100

    Delete dev.js

--- config/dev.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  googleClientID:
-    '192697751251-omtrpnig7oo2pra2ml08d709ufnfda4d.apps.googleusercontent.com',
-  googleClientSecret: 'PvCkTVJR4klGlfqldMEsDEYH',
-  mongooseURI: 'mongodb://localhost:27017/emaily',
-  cookieKey: 'babajamotokusecretcookie',
-};
-
-//
