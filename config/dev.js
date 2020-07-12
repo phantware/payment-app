f63043459c68f862079199d44656ec03fc4e8b90
phantware <phantmoney2011@gmail.com>
2020-07-12 21:50:03 +0100

    config folder modified

--- config/dev.js
+++ config/dev.js
@@ -2,9 +2,8 @@ module.exports = {
   googleClientID:
     '192697751251-omtrpnig7oo2pra2ml08d709ufnfda4d.apps.googleusercontent.com',
   googleClientSecret: 'PvCkTVJR4klGlfqldMEsDEYH',
-  mongooseURI:
-    'mongodb+srv://phantware22:jibaskido86@cluster0.nusat.mongodb.net/emaily?retryWrites=true&w=majority',
+  mongooseURI: 'mongodb://localhost:27017/emaily',
   cookieKey: 'babajamotokusecretcookie',
 };
 
-// mongodb://localhost:27017/emaily
+//
