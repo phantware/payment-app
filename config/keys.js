afd3d6d66617555cef0f2678d6cd764174fc0e2d
phantware <phantmoney2011@gmail.com>
2020-07-12 22:58:43 +0100

    config file adjusted

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 //Check if we are int production or in development environment
 
-if (process.env.Node_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
   // We are now in production mode
   module.exports = require('./prod');
 } else {
